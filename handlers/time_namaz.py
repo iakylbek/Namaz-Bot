89af0b7bca9e874cf1c6fe9e7ea4e30ec9e39b86
iakylbek <iakylbek005@gmail.com>
2024-12-01 16:09:14 +0300

    refactoring time namaz

--- handlers/time_namaz.py
+++ handlers/time_namaz.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from aiogram.types import Message
 
 namaz_times = [
-        ["\U0001F305 Фаджр", "05:20"],
-        ["\U0001F54C Зухр", "12:30"],
-        ["\U0001F3D9 Аср", "15:45"],
-        ["\U0001F304 Магриб", "18:10"],
-        ["\U0001F303 Иша", "19:30"]
+        ["\U0001F305 Фаджр", "05:20", "\U00002705"],
+        ["\U0001F54C Зухр", "12:30", "\U00002705"],
+        ["\U0001F3D9 Аср", "15:45", "\U0000274c"],
+        ["\U0001F304 Магриб", "18:10", "\U00002705"],
+        ["\U0001F303 Иша", "19:30", "\U0001f55c"]
     ]
 
 async def time_namaz_handler(message: Message) -> None:
@@ -15,9 +15,10 @@ async def time_namaz_handler(message: Message) -> None:
         f"Время намазов на *{formatted_date}*\n"
         "В городе Москва\n\n"
     )
-
-    for name, time in namaz_times:
-        text += f"• *{name:<10}:* {time}\n"
+    text += '`'
+    for name, time, status in namaz_times:
+        text += f"{name:<8}: {time} {status}\n"
+    text += '`'
     
     text += "\*всемирная исламская лига"
 
